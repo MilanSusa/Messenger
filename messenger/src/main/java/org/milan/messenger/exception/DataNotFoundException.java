a153b27c060284573a5fd4dd87ea37bffb6bd8d8
Milan <Milan@Milan-PC>
2017-03-12 17:42:18 +0100

    Inital checkin

---
+++ messenger/src/main/java/org/milan/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,13 @@
+package org.milan.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6641474935093860756L;
+
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+}
